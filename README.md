62a9b4a2044fd4dec93cdf53ffb585df93dfa0f7
Juan Matthys Uys <opyate@gmail.com>
2014-07-09 14:16:17 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# What?
+
+Scaffolds Python Flask [skeleton app]().
+
+# How?
+
+    curl https://raw.githubusercontent.com/landregistry/flask-example-scaffold/master/scaffold.sh | sh
+
+It will then ask you for your app name.
+
---
+++ scaffold.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "App name? (no spaces)"
+read APPNAME
+export APPNAME
+
+TEMP=$(mktemp -d -t python-flask-scaffold-XXXXXX) 
+curl -L https://github.com/LandRegistry/flask-examples/archive/master.zip -o $TEMP/master.zip
+unzip $TEMP/master -d $TEMP
+
+mv $TEMP/flask-examples-master $APPNAME
+
+echo "Renaming the application proper"
+mv $APPNAME/appname $APPNAME/$APPNAME
+
+for f in $(find $APPNAME -type f) ; do
+  perl -pi -e 's/appname/$ENV{APPNAME}/g' $f
+done
+
+echo "Done. App at $(pwd)/$APPNAME"
+echo
+echo ">> TODO"
+echo
+echo "cd $APPNAME"
+echo "virtualenv .virtualenv"
+echo "source .virtualenv/bin/activate"
+echo "pip install -r requirements.txt"
+echo "./run.sh"
+
+rm -rf $TEMP
